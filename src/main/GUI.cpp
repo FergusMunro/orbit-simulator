c35a22bfc1336f953b772ffb07bbc2864bd176bd
FergusMunro <nintendoisnotpoo@outlook.com>
2024-11-20 17:06:04 +0000

    removed jermaface

--- src/main/GUI.cpp
+++ src/main/GUI.cpp
@@ -37,40 +37,9 @@ void GUI::run() {
   ISceneManager *smgr = device->getSceneManager();
   IGUIEnvironment *guienv = device->getGUIEnvironment();
 
-  guienv->addStaticText(L"first gui edition of orbit simulator",
-                        rect<s32>(10, 10, 260, 22), true);
-
-  ISceneNode *sphere = smgr->addSphereSceneNode();
-
-  sphere->setScale(vector3df(5, 5, 5));
-  sphere->setPosition(vector3df(0, 0, 30));
-  sphere->setMaterialFlag(EMF_LIGHTING, false);
-  sphere->setMaterialTexture(0, driver->getTexture("assets/jermajpg"));
-
-  smgr->addCameraSceneNode(0, vector3df(0, 0, -40), vector3df(0, 0, 30));
-
-  vector3df center = sphere->getPosition();
-
-  vector3df velocity = vector3df(1, 1, 2);
-
-  vector3df acceleration = vector3df(0, 0, 0);
-
-  vector3df d;
-
   while (device->run()) {
 
     driver->beginScene(true, true, SColor(255, 100, 101, 140));
-
-    d = sphere->getPosition() - center;
-
-    acceleration = -0.02 * d;
-
-    velocity = velocity + acceleration;
-
-    sphere->setPosition(sphere->getPosition() + velocity);
-
-    sphere->setRotation(sphere->getRotation() + vector3df(0.5f, 1.0f, 0.0f));
-
     smgr->drawAll();
     guienv->drawAll();
 
